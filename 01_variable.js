b43d6046763d61d1540f1b0be85959ced737ceb1
Dhandoretejal <tejaldhandore2000@gmail.com>
2022-12-15 22:17:29 +0530

    01_variable.js
    01_variable.js

---
+++ 01_variable.js
@@ -0,0 +1,20 @@
+undefined="undefined";
+console.log(undefined);
+var myName; // variable declration
+myName="Tejal Dhandore"; //variable initialzation
+console.log(myName);
+
+var myFullName ="Tejal Suresh Dhandore";
+console.log(myFullName); //variable declration and initialzation at the same time
+
+var mylove="I love only JS";
+console.log(mylove);
+ 
+var myName="Tejal Dhandore";
+var Age ="22";
+var Address="Pune";
+var Pin_code="413010";
+var Gender="Female";
+var is_Marride="No";
+console.log(myName,Age,Address,Pin_code,Gender,is_Marride);
+
