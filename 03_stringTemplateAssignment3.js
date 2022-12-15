7979679b680c1feb9bc7aa151e1f3c38a808274f
Dhandoretejal <tejaldhandore2000@gmail.com>
2022-12-15 22:23:00 +0530

    03_stringTemplateAssignment3.js
    03_stringTemplateAssignment3.js

---
+++ 03_stringTemplateAssignment3.js
@@ -0,0 +1,36 @@
+console.log(`============String value as Argument=================`);
+var stringHandsOn = function () {
+  myString = "    The length of word JavaScript is =10.& its square is=100  ";
+  console.log(` ${myString} \n`);
+};
+stringHandsOn();
+var stringHandsOn = function () {
+  myString =
+    "    The length of word Google chrome is =13.& its square is=169  ";
+  console.log(` ${myString} \n`);
+};
+stringHandsOn();
+var stringHandsOn = function () {
+  myString =
+    "    The length of word Developer Smart is =15.& its square is=225  ";
+  console.log(` ${myString} \n`);
+};
+stringHandsOn();
+console.log(
+  `============Function Expression with No Argument & No Return ==================`
+);
+var stringHandsOn = function () {
+  myString =
+    "   2.1  length of Stringis =22. Total no of words in string are=4  After dividing String length by total no of word is=5.5";
+  console.log(` ${myString} \n`);
+};
+stringHandsOn();
+console.log(
+  `=============================================================================================`
+);
+
+var stringHandsOn = function () {
+  myString = "    2.2 Multiplying string length with total words =88.  ";
+  console.log(` ${myString} \n`);
+};
+stringHandsOn();
